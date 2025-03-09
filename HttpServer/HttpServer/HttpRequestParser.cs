b4c8e2bb021908e3dae43c9394bf75a9396ef4d1
Owendho <owendho2000@gmail.com>
2025-03-09 12:54:34 +0100

    Created Http request parser class

---
+++ HttpServer/HttpServer/HttpRequestParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer
+{
+    class HttpRequestParser
+    {
+        //Receive http request from browser
+        //Setup server
+        //Need an ip address that the browser can use to send requests. could be localhost
+
+        //Ip address method
+
+        //Http parser method
+
+    }
+}
